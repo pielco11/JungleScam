47c8bf215ffbacf983a1c0e751e1e8a9a13351d1
Francesco Poldi <francesco.poldi@protonmail.ch>
2018-12-02 16:07:15 +0100

    Added colored output

--- scraper.py
+++ scraper.py
@@ -8,12 +8,40 @@ import os
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
+# setup colored output
+from colorama import init
+init()
+from colorama import Fore, Back, Style
+
 IDs = {}
 _ndivs = 0
 
-baseUrl = sys.argv[1]
-pages = sys.argv[2]
-filename = sys.argv[3]
+#baseUrl = sys.argv[1]
+#pages = sys.argv[2]
+#filename = sys.argv[3]
+
+print (Fore.YELLOW + """
+888888                            888           .d8888b.
+    "88b                            888          d88P  Y88b
+     888                            888          Y88b.
+     888 888  888 88888b.   .d88b.  888  .d88b.   "Y888b.    .d8888b  8888b.  88888b.d88b.
+     888 888  888 888 "88b d88P"88b 888 d8P  Y8b     "Y88b. d88P"        "88b 888 "888 "88b
+     888 888  888 888  888 888  888 888 88888888       "888 888      .d888888 888  888  888
+     88P Y88b 888 888  888 Y88b 888 888 Y8b.     Y88b  d88P Y88b.    888  888 888  888  888
+     888  "Y88888 888  888  "Y88888 888  "Y8888   "Y8888P"   "Y8888P "Y888888 888  888  888
+   .d88P                        888
+ .d88P"                    Y8b d88P
+888P"                       "Y88P"
+""")
+print (Fore.CYAN + 'An Amazon OSINT scraper for potential scam accounts')
+print (Fore.YELLOW + 'By @jakecreps & @noneprivacy')
+print (Fore.CYAN + 'Insert your URL')
+baseUrl = input()
+print ('How many pages do you want to scan?')
+pages = input()
+print ('What do you want to call the csv?')
+filename = input()
+
 
 site = "https://" + baseUrl.split('//')[1].split('/')[0]
 http = urllib3.PoolManager( 10,
